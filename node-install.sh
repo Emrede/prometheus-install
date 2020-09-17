7b2a5a3797227d1b9e4ae599d97c422033bb39a8
Emre Demir <emrede@live.com>
2020-09-17 18:55:20 +0300

    loading bar

--- node-install.sh
+++ node-install.sh
@@ -5,6 +5,15 @@ echo "#############################################################"
 echo "# === Prometheus Node Exporter Installation for Ubuntu  === #"
 echo "#############################################################"
 
+loading(){
+    echo -ne '#####                     (33%)\r'
+    sleep 1
+    echo -ne '#############             (66%)\r'
+    sleep 1
+    echo -ne '#######################   (100%)\r'
+    echo -ne '\n'
+}
+
 cd /tmp
 wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
 tar xvfz node_exporter-1.0.1.linux-amd64.tar.gz
@@ -21,13 +30,7 @@ sudo chmod 644 /etc/systemd/system/node-exporter.service
 sudo systemctl daemon-reload
 sudo systemctl start node-exporter.service
 
-echo -ne '#####                     (33%)\r'
-sleep 1
-echo -ne '#############             (66%)\r'
-sleep 1
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
-
+loading
 # sudo systemctl status node-exporter.service
 
 echo "# === Installation completed  ! === #"
